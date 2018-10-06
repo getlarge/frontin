e2f111a154b746e14b2f43944f6a2c56cadf4779
getlarge <ed@getlarge.eu>
2018-10-06 22:50:35 +0200

    configfile ok

--- src/configFile.js
+++ src/configFile.js
@@ -1,41 +1,41 @@
-const clientId = process.env.MQTT_CLIENT_ID || "frontin";
+const mqttClientId =
+  (process.env.MQTT_CLIENT_ID || 'frontin') +
+  '_' +
+  Math.random()
+    .toString(16)
+    .substr(2, 8);
 const mqttClientPass = process.env.MQTT_CLIENT_PASS;
+const mqttUser = process.env.MQTT_CLIENT_USER || '';
+const mqttUrl = process.env.MQTT_CLIENT_URL || 'mqtt://localhost:3001';
 
 const config = {
-    appname: process.env.APP_NAME || "Frontin",
-    prefix: process.env.PREFIX || "getlarge",
-    homeLink: "/",
-    //localhost: process.env.LOCALMACHINE || "localhost",
-    localhost: "localhost",
-    vueDevtoolsPort: process.env.VUE_DEVTOOLS_PORT || "8098",
-    gitLink: process.env.GIT_LINK || "https://framagit.org/getlarge/frontin/",
-    ComponentsPath: process.env.COMPONENTS_PATH || "tree/master/src/components",
-    tel: process.env.PHONE || "+33624297761",
-    domain: process.env.DOMAIN || "getlarge.eu",
-    httpClient: {
-        url: process.env.HTTP_CLIENT_URL || "http://localhost:3000/",
-        username: process.env.HTTP_CLIENT_USER || "",
-        password: process.env.HTTP_CLIENT_PASS || ""
+  appname: process.env.APP_NAME || 'Frontin',
+  prefix: process.env.PREFIX || 'getlarge',
+  homeLink: '/',
+  //localhost: process.env.LOCALMACHINE || "localhost",
+  localhost: 'localhost',
+  vueDevtoolsPort: process.env.VUE_DEVTOOLS_PORT || '8098',
+  gitLink: process.env.GIT_LINK || 'https://framagit.org/getlarge/frontin/',
+  ComponentsPath: process.env.COMPONENTS_PATH || 'tree/master/src/components',
+  tel: process.env.PHONE || '+33624297761',
+  domain: process.env.DOMAIN || 'getlarge.eu',
+  httpClient: {
+    url: process.env.HTTP_CLIENT_URL || 'http://localhost:3000/',
+    username: process.env.HTTP_CLIENT_USER || '',
+    password: process.env.HTTP_CLIENT_PASS || '',
+  },
+  mqttClient: {
+    url: mqttUrl,
+    options: {
+      clientId: mqttClientId,
+      username: mqttUser,
+      password: new Buffer(mqttClientPass) || '',
     },
-    mqttClient: {
-        url: process.env.MQTT_CLIENT_URL || "mqtt://localhost:3001",
-        options: {
-            host: process.env.MQTT_CLIENT_HOST || "localhost",
-            port: process.env.MQTT_CLIENT_PORT || 3001,
-            clientId:
-                clientId +
-                "_" +
-                Math.random()
-                    .toString(16)
-                    .substr(2, 8),
-            username: process.env.MQTT_CLIENT_USER || "",
-            password: new Buffer(mqttClientPass) || ""
-        }
-    },
-    chatClient: {
-        url: process.env.CHAT_CLIENT_URL || "https://chat.aloes.io/livechat",
-        scriptUrl: process.env.CHAT_CLIENT_SCRIPT_URL || ""
-    }
+  },
+  chatClient: {
+    url: process.env.CHAT_CLIENT_URL || 'https://chat.aloes.io/livechat',
+    scriptUrl: process.env.CHAT_CLIENT_SCRIPT_URL || '',
+  },
 };
 
-export { config };
+export {config};
